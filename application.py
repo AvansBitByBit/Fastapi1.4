8262ce41d57e41430b173bd38bd44f6078b89a18
FabianoPungol <fl.pungol@student.avans.nl>
2025-06-12 19:15:10 +0200

    Test

--- application.py
+++ application.py
@@ -19,7 +19,7 @@ app = FastAPI()
 class Features(BaseModel):
     features: List[float]
 
-# om de applicatie te testen!
+# om de applicatie te testen
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
