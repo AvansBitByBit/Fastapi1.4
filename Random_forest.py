d1979bf83aac362e2781b2e39b08249ca8c1c7d7
Rishyan Syal <karanrishyan@gmail.com>
2025-06-24 00:03:17 +0200

    predict on temprature

--- Random_forest.py
+++ Random_forest.py
@@ -3,11 +3,12 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 
 def process_dataset(data_list):
-    X = [item["temperature"] for item in data_list]
+    X = [[item["temperature"]] for item in data_list]  # 2D array: (n_samples, 1)
     y = [item["location"] for item in data_list]
-    return X, y
+    return np.array(X), np.array(y)
 
 def train_random_forest(X, y):
     model = RandomForestClassifier()
     model.fit(X, y)
-    return models
+    return model
+
